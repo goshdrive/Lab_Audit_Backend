3bee5534afb51b591b97c05adff51f09bd401f10
HongkangTian <thk521478963@gmail.com>
2021-03-25 21:10:54 +0000

    connect to atlas

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/goshDrive'
+    'mongoUrl': 'mongodb+srv://Alen:a123456@cluster0.krdxf.mongodb.net/goshDrive?retryWrites=true&w=majority'
 }
 
