b3755e7b68a08518275ac9d9c9301e34fa0b0a2b
mitrotasios <a.mitrotasios13@gmail.com>
2021-04-05 00:49:59 +0100

    added to cors

--- routes/cors.js
+++ routes/cors.js
@@ -2,7 +2,9 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-const whitelist = ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:8100', 'capacitor://localhost', 'http://localhost', 'https://gentle-moss-001c37603.azurestaticapps.net/'];
+const whitelist = ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:8100', 'capacitor://localhost', 
+                    'http://localhost', 'https://gentle-moss-001c37603.azurestaticapps.net/',
+                    'https://kind-water-02d950403.azurestaticapps.net/'];
 
 var corsOptionsDelegate = (req, callback) => {
     var corsOptions;
